3cfef75c3ea8a563552bc277c14592fb53af9f86
Pablo Anglat <pablo.anglat@globant.com>
2020-06-03 20:29:30 -0300

    Add Route and Home

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import './styles.scss';
 import Board from '../app-components/Board';
 import { CreateGame } from '../../business/Game/actions';
+import Home from '../app-components/Home';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
@@ -10,7 +12,18 @@ const App: React.FC = () => {
     dispatch(CreateGame({ cols: 10, rows: 8, bombs: 5 }));
   });
 
-  return <Board />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/board">
+          <Board />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/app-components/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.scss';
+
+const Home: React.FC = () => <div className="home">Home</div>;
+
+export default Home;
