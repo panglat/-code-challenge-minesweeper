4b7f943b30ceaa49913b5d6ece9d09b778725326
Pablo Anglat <pablo.anglat@globant.com>
2020-06-07 12:17:02 -0300

    Move Redux Provider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import GameSetup from 'components/app-components/GameSetup';
+import GameBoard from 'components/app-components/GameBoard';
+import configureStore from 'store/configureStore';
+
 import './styles.scss';
-import GameSetup from '../app-components/GameSetup';
-import GameBoard from '../app-components/GameBoard';
+
+const store = configureStore();
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/board">
-          <GameBoard />
-        </Route>
-        <Route path="/">
-          <GameSetup />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/board">
+            <GameBoard />
+          </Route>
+          <Route path="/">
+            <GameSetup />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import configureStore from './store/configureStore';
+import App from 'components/App';
 
 import './index.css';
 
-const store = configureStore();
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
