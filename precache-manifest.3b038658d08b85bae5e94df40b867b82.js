0ca3d5b04c438ca6872c4f491cd0990a823daa3e
Pablo Anglat <pablo.anglat@globant.com>
2020-06-23 02:08:59 -0300

    Updates

---
+++ precache-manifest.3b038658d08b85bae5e94df40b867b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbfd4f9777c3bf2d4834ccd04ab6f8b",
+    "url": "/code-challenge-minesweeper/index.html"
+  },
+  {
+    "revision": "86eedf9acaeab59a4e29",
+    "url": "/code-challenge-minesweeper/static/css/main.621ba3c9.chunk.css"
+  },
+  {
+    "revision": "cb8d135970577c519d4f",
+    "url": "/code-challenge-minesweeper/static/js/2.48ef348a.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/code-challenge-minesweeper/static/js/2.48ef348a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86eedf9acaeab59a4e29",
+    "url": "/code-challenge-minesweeper/static/js/main.63370939.chunk.js"
+  },
+  {
+    "revision": "9881c4f4491ca262a00c",
+    "url": "/code-challenge-minesweeper/static/js/runtime-main.1ca286e8.js"
+  }
+]);
